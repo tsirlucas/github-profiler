2cd10667388a9e8a074070422085e63c9032f72e
lucascorreia <tsirlucas@gmail.com>
2017-05-04 22:33:15 -0300

    Inlining api functions

--- src/helpers/firebase-api.js
+++ src/helpers/firebase-api.js
@@ -14,21 +14,14 @@ const request = (user, method, params = {}, noteId) => {
         config.body = JSON.stringify(params)
     }
 
-    console.log(params);
-    return fetch(`https://github-profiler-1ac46.firebaseio.com/${user}/notes/${noteId || ''}.json?format=export`, config)};
-
-export const listNotes = (user) => {
-    return request(user, 'GET');
+    return fetch(`https://github-profiler-1ac46.firebaseio.com/${user}/notes/${noteId || ''}.json?format=export`, config)
 };
 
-export const addNote = (user, params) => {
-    return request(user, 'PATCH', params);
-};
+export const listNotes = (user) => request(user, 'GET');
 
-export const editNote = (user, params) => {
-    return request(user, 'PATCH', params);
-};
+export const addNote = (user, params) => request(user, 'PATCH', params);
 
-export const removeNote = (user, params) => {
-    return request(user, 'DELETE', params, params.id);
-};
+
+export const editNote = (user, params) => request(user, 'PATCH', params);
+
+export const removeNote = (user, params) => request(user, 'DELETE', params, params.id);
