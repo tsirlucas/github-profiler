7d542a5a3748a73142d8a1f372fb1d86df7f0640
lucascorreia <tsirlucas@gmail.com>
2017-06-04 00:43:16 -0300

    Autoreload on sw update and tabs animation

--- src/util/InitSW.js
+++ src/util/InitSW.js
@@ -2,8 +2,18 @@ export default () => {
 	if ('serviceWorker' in navigator && location.protocol === 'https:') {
 		console.log('Starting service worker...');
 		navigator.serviceWorker.register('/github-profiler/service-worker.js', {scope: './'})
-			.then(() => {
+			.then((reg) => {
 				console.log('Done. Now you\'re running offline.');
+				reg.onupdatefound = () => {
+					console.log('Updating service worker...');
+					const installingWorker = reg.installing;
+					installingWorker.onstatechange = () => {
+						if (installingWorker.state === 'installed' && navigator.serviceWorker && navigator.serviceWorker.controller) {
+							console.log('Done! Reloading page...');
+							window.location.reload();
+						}
+					};
+				};
 			})
 			.catch((err) => {
 				console.log(err);
